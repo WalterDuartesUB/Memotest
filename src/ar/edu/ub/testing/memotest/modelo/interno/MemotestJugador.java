af8a425301e6ee4b6ab922eaad5d6f54be5e3ba1
WalterDuartesUB <37937023+WalterDuartesUB@users.noreply.github.com>
2018-04-08 11:10:13 -0300

    Se agrega el uso de excepciones para el metodo setProximoJugador

--- src/ar/edu/ub/testing/memotest/modelo/interno/MemotestJugador.java
+++ src/ar/edu/ub/testing/memotest/modelo/interno/MemotestJugador.java
@@ -1,11 +1,17 @@
 package ar.edu.ub.testing.memotest.modelo.interno;
 
 import ar.edu.ub.testing.memotest.modelo.Jugador;
+import ar.edu.ub.testing.memotest.modelo.interno.exception.MemotestJugadorProximoEsMismoJugadorException;
+import ar.edu.ub.testing.memotest.modelo.interno.exception.MemotestJugadorProximoEsNullException;
 
 /**
  * MemotestJugador representa a un Jugador dentro del Memotest
  * @author wduartes
  *
+ * Historial
+ * ---------
+ * 2018/04/08	wduartes	Aparecen las excepciones al tratar de 
+ * 							settear como proximo jugador a si mismo o null
  */
 public class MemotestJugador extends Jugador 
 {
@@ -38,6 +44,12 @@ public class MemotestJugador extends Jugador
 
 	public void setProximoJugador(MemotestJugador proximoJugador) 
 	{
+		if( proximoJugador == null )
+			throw new MemotestJugadorProximoEsNullException();
+		
+		if( this == proximoJugador )
+			throw new MemotestJugadorProximoEsMismoJugadorException();
+		
 		this.proximoJugador = proximoJugador;
 	}	
 }
