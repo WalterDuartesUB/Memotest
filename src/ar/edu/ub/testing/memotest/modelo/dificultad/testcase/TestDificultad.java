a81931b0ae511071cc7ed6a07550940e0abadbbf
WalterDuartesUB <37937023+WalterDuartesUB@users.noreply.github.com>
2018-04-08 00:17:14 -0300

    Se agregan los TestCase en Dificultad para el campo columna

--- src/ar/edu/ub/testing/memotest/modelo/dificultad/testcase/TestDificultad.java
+++ src/ar/edu/ub/testing/memotest/modelo/dificultad/testcase/TestDificultad.java
@@ -1,11 +1,16 @@
 package ar.edu.ub.testing.memotest.modelo.dificultad.testcase;
 
 import ar.edu.ub.testing.memotest.modelo.dificultad.Dificultad;
+import ar.edu.ub.testing.memotest.modelo.dificultad.exception.DificultadSinCantidadColumnasException;
 import ar.edu.ub.testing.memotest.modelo.dificultad.exception.DificultadSinNombreException;
 import junit.framework.TestCase;
 
 public class TestDificultad extends TestCase
 {
+	
+	///////////////////////////////////////////////////////////////////////////
+	// Pruebo el campo nombre
+	
 	public void testDificultadNombreNull()
 	{
 		try
@@ -40,5 +45,54 @@ public class TestDificultad extends TestCase
 		{
 			
 		}		
-	}		
+	}
+	
+	
+	public void testDificultadNombre()
+	{	
+		String nombre = "dificultad";
+		Dificultad dificultad = new Dificultad( nombre, 1, 2 );
+		assertEquals( nombre , dificultad.getNombre() );		
+	}	
+	
+	///////////////////////////////////////////////////////////////////////////
+	// Pruebo el campo columna
+	
+	
+	public void testDificultadColumnasNull()
+	{
+		try
+		{
+			assertNull( new Dificultad( "dificultad", 1, null ) );
+		} 
+		catch (DificultadSinCantidadColumnasException e)
+		{
+			
+		}		
+	}
+	
+	public void testDificultadColumnasCero()
+	{
+		try
+		{
+			assertNull( new Dificultad( "dificultad", 1, 0 ) );
+		} 
+		catch (DificultadSinCantidadColumnasException e)
+		{
+			
+		}		
+	}	
+	
+	public void testDificultadColumnasNegativo()
+	{
+		try
+		{
+			assertNull( new Dificultad( "dificultad", 1, -2 ) );
+		} 
+		catch (DificultadSinCantidadColumnasException e)
+		{
+			
+		}		
+	}
+		
 }
